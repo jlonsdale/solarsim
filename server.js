8b3ec947c15fa85209dd8d466fe6db5b206923a9
Jessie <lonsdale.jess@gmail.com>
2020-01-12 13:50:56 +0000

    deployment debugging

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const { JSDOM } = jsdom;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
+app.listen(8000);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + 'public/index.html')
